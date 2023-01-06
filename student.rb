367e8d8ec875b1b50dfd87c8bc09111d234c8a70
Olivier-Kango <olivierkango@gmail.com>
2023-01-06 09:46:04 -0800

    Correct class Student

--- student.rb
+++ student.rb
@@ -1,11 +1,8 @@
 require './person'
 
 class Student < Person
-  attr_reader :parent_permission
-  attr_accessor :classroom, :name, :age
-
-  def initialize(classroom, age, name = 'Unknown', parent_permission = true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
