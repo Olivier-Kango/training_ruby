72babdf26f644c99eea4f529159e5a6ac49d1f48
Olivier-Kango <olivierkango@gmail.com>
2023-01-09 11:49:46 -0800

    Initialize Teacher

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,9 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+  attr_accessor :specialization
+
+  def initialize(age, name = 'Unknown', specialization, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
