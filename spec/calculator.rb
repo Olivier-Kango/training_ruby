b82415320368b36e76963baa2cd3388da35e8bb4
Olivier-Kango <olivierkango@gmail.com>
2023-01-11 13:49:15 -0800

    Add the calculator

---
+++ spec/calculator.rb
@@ -0,0 +1,17 @@
+class Calculator
+  def add(x, y)
+    x + y
+  end
+  def subtract(x, y)
+    x - y
+  end
+  def multiply(x, y)
+    x * y
+  end
+  def divide(x, y)
+    x / y
+  end
+  def modulo(x, y)
+    x % y
+  end
+end
