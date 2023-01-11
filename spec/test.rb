da9cc4816285131a18571615b512099555035d14
Olivier-Kango <olivierkango@gmail.com>
2023-01-11 13:49:26 -0800

    Test the calculator

--- spec/test.rb
+++ spec/test.rb
@@ -1,3 +1,6 @@
+
+require "calculator"
+
 class Dog
   attr_reader :good_dog, :has_been_walked
 
@@ -216,3 +219,39 @@ end
 #    books2 = lib2.books.map { |book| book.title }
 #    books.should eql books2
 # end
+
+
+describe Calculator do
+   context "Given two numbers" do
+        it "can add the numbers using the add method" do
+             calc = Calculator.new
+             x = 13
+             y = 5
+             expect(calc.add(x, y)).to eq(18)
+        end
+        it "can subtract the numbers using the subtract method" do
+             calc = Calculator.new
+             x = 13
+             y = 5
+             expect(calc.subtract(x, y)).to eq(8)
+        end
+        it "can multiply the numbers using the multiply method" do
+             calc = Calculator.new
+             x = 13
+             y = 5
+             expect(calc.multiply(x, y)).to eq(65)
+        end
+        it "can divide the numbers using the divide method" do
+             calc = Calculator.new
+             x = 13
+             y = 5
+             expect(calc.divide(x, y)).to eq(2)
+        end
+        it "can provide the remainder when dividing two numbers using the modulo method" do
+             calc = Calculator.new
+             x = 13
+             y = 5
+             expect(calc.modulo(x, y)).to eq(3)
+        end
+   end
+end
