ce0d8c751e1e18ba638d9ec7652c3a34a1c214e0
Olivier-Kango <olivierkango@gmail.com>
2023-01-11 13:09:01 -0800

    class StringAnalyzer

---
+++ spec/string_analyzer.rb
@@ -0,0 +1,27 @@
+class StringAnalyzer 
+  def has_vowels?(str) 
+     !!(str =~ /[aeiou]+/i) 
+  end 
+end
+
+
+# Class/Type Matchers
+
+# describe "An example of the type/class Matchers" do
+ 
+#   it "should show how the type/class Matchers work" do
+#      x = 1 
+#      y = 3.14 
+#      z = 'test string' 
+     
+#      # The following Expectations will all pass
+#      expect(x).to be_instance_of Fixnum 
+#      expect(y).to be_kind_of Numeric 
+#      expect(z).to respond_to(:length) 
+#   end
+  
+# end
+
+
+
+
